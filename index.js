a7f0de111ff248210281ea66fc4b226a524b87fb
sravittxof <travis.fox90@gmail.come>
2021-04-28 21:24:57 -0400

    commit tree 944643e9f793a66fbcf612e6933b2312171b2b3d

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(visualFlair = "*"){
+    return function(innerParam = "special"){
+        return `You are ${visualFlair}${innerParam}${visualFlair}!`;
+    }
+}
+
+const encouragingPromptFunction = wrapAdjective("!!!");
+
+encouragingPromptFunction();
+
+const Calculator = {
+    add: function(num1, num2){ return num1 + num2; },
+    subtract: function(num1, num2){ return num1 - num2; },
+    multiply: function(num1, num2){ return num1 * num2; },
+    divide: function(num1, num2){ return num1 / num2; },
+};
+
+function actionApplyer(startingInt, functionArray){
+    let currentValue = startingInt;
+    for (const elem of functionArray) {
+        currentValue = elem(currentValue);
+    }
+    return currentValue;
+}
